3d7db4d772dcb985d37d056a30de188253be798b
Mohamed Elnagar <mohamedsaadyelnagar@gmail.com>
2024-01-05 17:57:17 +0200

    handle delete todo

--- components/TodoList/TodoList.js
+++ components/TodoList/TodoList.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Checkbox } from 'antd';
 import './todos.css';
+import { DeleteOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { useDispatch } from "react-redux";
+import { deleteTodo } from '../Todo/todoSlice'
 
 function TodoList() {
   const { todoList } = useSelector((state) => state.todo);
-  const dispatch = useDispatch();
-  const items = [];
 
+  const dispatch = useDispatch();
+
+  const handleDeleteTodo = (todo) => {
+    dispatch(deleteTodo(todo.id))
+  };
   const onChange = (e) => {
     console.log(`checked = ${e.target.checked}`);
   };
-  console.log(todoList);
+
 
   return <div>
-    <ul>{todoList.map((todo) => (
-      <li key={todo.id}><Checkbox id={todo.id} name={todo.name} onChange={onChange}/> {todo.name} </li>
+    <ul>{todoList.map((todo, index) => (
+      <li key={todo.id}><Checkbox id={todo.id} name={todo.name} onChange={onChange} /> {todo.name}
+        <Button className="delete"  onClick={() => handleDeleteTodo(todo)}><DeleteOutlined size="medium" /></Button></li>
     ))}</ul>
   </div>;
 }
