38a116f086bd11d7e958ce6ea596fc55dd7c5abc
Mohamed Elnagar <mohamedsaadyelnagar@gmail.com>
2024-01-05 18:53:26 +0200

    style todo list

--- components/TodoList/TodoList.js
+++ components/TodoList/TodoList.js
@@ -1,15 +1,12 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Checkbox } from 'antd';
+import { useSelector, useDispatch } from "react-redux";
+import { Checkbox, Button, Flex, Typography } from 'antd';
 import './todos.css';
 import { DeleteOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
-import { useDispatch } from "react-redux";
 import { deleteTodo, makeTodoCompleted } from '../Todo/todoSlice'
 
 function TodoList() {
   const { todoList } = useSelector((state) => state.todo);
-
+  const { Text } = Typography;
   const dispatch = useDispatch();
 
   const handleDeleteTodo = (todo) => {
@@ -22,11 +19,14 @@ function TodoList() {
   console.log(todoList)
 
   return <div>
-    <ul>{todoList.map((todo, index) => (
-      <li key={todo.id}><Checkbox id={todo.id} name={todo.name} onChange={() => handleCompletedTask(todo)}  />
-        <span className={todo.completed === true ? 'completed' : ''}>{todo.name}</span>
-        <Button className="delete" onClick={() => handleDeleteTodo(todo)}><DeleteOutlined size="medium" /></Button></li>
-    ))}</ul>
+    <div>{todoList.map((todo, index) => (
+      <Flex key={todo.id} justify="space-between">
+        <Flex >
+          <Checkbox id={todo.id} name={todo.name} onChange={() => handleCompletedTask(todo)} />
+          <Text className={`todo ${todo.completed === true ? 'completed' : ''}`}>{todo.name}</Text>
+        </Flex>
+        <Button className="delete" onClick={() => handleDeleteTodo(todo)}><DeleteOutlined size="medium" /></Button></Flex>
+    ))}</div>
   </div>;
 }
 
